67ab42d4b2612a87887e5e9e638a8ca7625ead5d
Brahan Quispe <brahan.quispe@tecsup.edu.pe>
2026-06-18 11:07:31 -0500

    raaa

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,51 @@ export default function Home() {
     <>
       <Head>
         <title>Mi Sitio Optimizado - Home</title>
-        <meta name="description" content="Aprende sobre optimización SEO y rendimiento en Next.js." />
-        <meta name="keywords" content="Next.js, SEO, optimización web" />
-        <meta property="og:title" content="Mi Sitio Optimizado" />
-        <meta property="og:description" content="Descubre técnicas avanzadas para mejorar tu web con Next.js." />
-        <meta property="og:image" content="https://lab14-j6vg.onrender.com/images/seo-image.png" />
-        <meta property="og:type" content="website" />
+
+        <meta
+          name="description"
+          content="Aprende sobre optimización SEO y rendimiento en Next.js."
+        />
+
+        <meta
+          name="keywords"
+          content="Next.js, SEO, optimización web"
+        />
+
+        <meta
+          name="google-site-verification"
+          content="v1C5Bz7JbLNWAFtcLPcUJBtIyKPZUmYOrVq7ZEvKNmY"
+        />
+
+        <meta
+          property="og:title"
+          content="Mi Sitio Optimizado"
+        />
+
+        <meta
+          property="og:description"
+          content="Descubre técnicas avanzadas para mejorar tu web con Next.js."
+        />
+
+        <meta
+          property="og:image"
+          content="https://lab14-j6vg.onrender.com/images/seo-image.png"
+        />
+
+        <meta
+          property="og:type"
+          content="website"
+        />
+
+        <meta
+          property="og:site_name"
+          content="Laboratorio SEO Next.js"
+        />
+
+        <meta
+          name="twitter:card"
+          content="summary_large_image"
+        />
       </Head>
 
       <h1>Bienvenido a mi página optimizada</h1>
